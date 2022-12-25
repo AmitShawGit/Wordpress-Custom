fe0cb0f176494feb3fa9d637fe347cf51a1f4ca5
Amit Shaw <amitshaw1421@gmail.com>
2022-12-25 17:20:02 +0530

    Row updated

--- index.php
+++ index.php
@@ -1,36 +1,25 @@
 <?php get_header() ?>
  
 
-        <div class="container">
-            <br>
+     
+            <div class="container">
             <div class="row">
-                <!--Image Section-->
-                <div class="col-md-6">
-                   
-                <?php
-            while(have_posts()) {the_post()
+            <?php
+                    while(have_posts()) {the_post()
+                
                  ?>
-             <div class="title"> <?php the_title(); ?> </div>
-              <?php $imagepath=wp_get_attachment_image_src(get_post_thumbnail_id(),'thumbnail');?>
-               <div class="pimg"><img src="<?php echo $imagepath[0]?>" class="img-fluid" alt=""></div>
-              
-              <?php
-               }
-                ?>
 
-             </div>
-                <!-- Content Section -->
-                <div class="col-md-6">
-              <?php while(have_posts()) {the_post()
-              ?>
-              <br><br><br><br>
-                <div class="about-content"></a><?php the_content(); ?><a href="<?php the_permalink(); ?>"><input type="button" name="" id="" value="Read more">
-                </div>
-               <?php } ?>
-
-                </div>
-
-                <!--Row Ends-->
+                 <div class="col-lg-6">
+                     <div class="title"> <?php the_title(); ?> </div>
+                      <?php $imagepath=wp_get_attachment_image_src(get_post_thumbnail_id(),'thumbnail');?>
+                   <div class="pimg"><img src="<?php echo $imagepath[0]?>" class="img-fluid" alt=""></div>
+                 </div>
+                   <div class="col-lg-6">
+                     <p><?php the_content(); ?></p>
+                     <a href="<?php the_permalink(); ?>" type="button" class="btn btn-primary">Read more</a>
+                   </div>
+                  <?php } ?>
+             <!--row ends-->
             </div>
             <!--Container Ends-->
         </div> 
